a0d7afb5f2c39b3517f0a140ca4292fbd00fef59
andygithub9 <andygithub9@gmail.com>
2022-04-08 15:57:15 +0800

    get data

---
+++ server/upload.js
@@ -0,0 +1,25 @@
+let http = require("http");
+let formidable = require("formidable");
+let fs = require("fs");
+const path = require("path");
+
+http
+  .createServer(function (req, res) {
+    //Create an instance of the form object
+    let form = new formidable.IncomingForm();
+
+    //Process the file upload in Node
+    form.parse(req, function (error, fields, file) {
+      let filepath = file.fileupload.filepath;
+      let newpath = __dirname + `\\upload\\`;
+      newpath += file.fileupload.originalFilename;
+
+      //Copy the uploaded file to a custom folder
+      fs.rename(filepath, newpath, function () {
+        //Send a NodeJS file upload confirmation message
+        res.write("NodeJS File Upload Success!");
+        res.end();
+      });
+    });
+  })
+  .listen(80);
